dd6a84a896056b894773d38536247c935c88e3dd
Anton Indyukov <indyukovanton@gmail.com>
2025-06-21 02:42:36 +0300

    ADD: modal form for added new title

--- frontend/src/js/components.js
+++ frontend/src/js/components.js
@@ -1,5 +1,8 @@
 import initTabs from "./components/tabs.js"
+import ModalWindow from "./components/modal_window.js"
 
 document.addEventListener('DOMContentLoaded', ()=> {
 	initTabs()
+
+	const modalWindow = new ModalWindow()
 })
